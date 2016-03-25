f7c60ff430bfc6d3c8a6df4f3fe91cc59a6a7646
Tjisana Kerr <tjisana@gmail.com>
2016-03-25 19:18:27 -0400

    remove

--- CS2/CSHELP/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-#include "stdafx.h"
-
-double expint(double x);
-void tridiagSolver(double *lowerD, double *diag, double *upperD, double *B, int nSize);
-
-
-#endif // 
