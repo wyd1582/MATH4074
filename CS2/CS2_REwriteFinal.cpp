c50077a36d471d30249697facbb2cdce7692a936
Tjisana Kerr <tjisana@gmail.com>
2016-03-26 14:29:37 -0400

    sync final

--- CS2/CS2_REwriteFinal.cpp
+++ CS2/CS2_REwriteFinal.cpp
@@ -1,16 +1,14 @@
 #include <math.h>
-//#include <sys/time.h>
+#include <sys/time.h>
 #include <time.h>
 #include "expint.hpp"
 #include "tridiagSolver.hpp"
 
 int main(int argc,char **argv){
 
-	/*struct timeval startTime, endTime; 
+	struct timeval startTime, endTime; 
 	long seconds, useconds; 
-	double mtime;*/
-	clock_t  t1;
-	t1 = clock();
+	double mtime;	
 	double   *L, *D, *U, *W,*Wadj, *x;
 	double   aA, bB;
 	double   ps0;
@@ -26,8 +24,8 @@ int main(int argc,char **argv){
 	double   sMin = 100.0, sMax = 4100.0;
 	double   mu=0.17,theta=-0.15;
 
-	int      N=1600;
-	int      M=400;
+	int      N=4000;
+	int      M=1000;
 
 	double   Dx = (log(sMax)-log(sMin))/N;
 	double   Dt = T/M;
@@ -46,16 +44,12 @@ int main(int argc,char **argv){
 		g[i]= new double[N+1];
 	}
 
-	//double   sig2_Dx = 1/(mu*lp*lp) *(1-exp(-lp*Dx)*(1+lp*Dx))+1/(mu*ln*ln) *(1-exp(-ln*Dx)*(1+ln*Dx));
-	double Y = 0;
-	double   sig2_Dx = (1 / mu)* pow(lp, Y - 2)* (-pow(lp*Dx, 1 - Y)*exp(-lp*Dx) + (1 - Y)*(exp(0.0) - exp(-lp*Dx)))
-		+ (1 / mu)* pow(ln, Y - 2)* (-pow(ln*Dx, 1 - Y)*exp(-ln*Dx) + (1 - Y)*(exp(0.0) - exp(-ln*Dx)));
-	//double   omega_Dx = 1/mu*(expint(lp*Dx)-expint(Dx*(lp-1)))+1/mu*(expint(ln*Dx)-expint(Dx*(ln+1)));
-	double   omega_Dx = (expint(lp*Dx) - expint((lp - 1)*Dx) + expint(ln*Dx) - expint((ln + 1)*Dx)) / mu;
+	double   sig2_Dx = 1/(mu*lp*lp) *(1-exp(-lp*Dx)*(1+lp*Dx))+1/(mu*ln*ln) *(1-exp(-ln*Dx)*(1+ln*Dx));	
+	double   omega_Dx = 1/mu*(expint(lp*Dx)-expint(Dx*(lp-1)))+1/mu*(expint(ln*Dx)-expint(Dx*(ln+1)));	
 	aA = sig2_Dx*Dt/(2.0*Dx*Dx);
 	bB = (rfr-div+ omega_Dx-0.5*sig2_Dx)*Dt/(2.0*Dx);
 
-	//gettimeofday(&startTime, NULL); 
+	gettimeofday(&startTime, NULL); 
 
 	// payoff
 	for ( i = 0; i <= N; ++i){
@@ -82,44 +76,19 @@ int main(int argc,char **argv){
 	double Bu = (aA + bB);
 	double r = rfr;
 	for (i = 1; i <=N-1; ++i){
-		if (i==1){
-			//L[i] = 0;
+		if (i==1){			
 			D[i] = 1 + 2.0*aA + rfr*Dt - (aA-bB)*       2.0/(1.0+Dx/2.0);
 			U[i] = -(aA+bB)            + (aA-bB)*(1.0-Dx/2)/(1.0+Dx/2.0);
-			// Boundary condition
-			//W[1] = W[1]+(aA-bB)*(K-exp(x[0]));
-			//W[1] = W[1]+(aA-bB)*(K*exp(-rfr*tau_j)-exp(x[0])*exp(-div*tau_j));
-
 		}
 		else if (i==N-1){
 			L[i] = -(aA-bB)              + (aA+bB)*(1+Dx/2.0)/(1.0-Dx/2.0);
-			D[i] = 1.0 + 2.0*aA + rfr*Dt - (aA+bB)*       2.0/(1.0-Dx/2.0);
-			//U[i] = 0;
-			// Boundary condition
-			//W[N-1] = W[N-1]+0.0;
+			D[i] = 1.0 + 2.0*aA + rfr*Dt - (aA+bB)*       2.0/(1.0-Dx/2.0);		
 		}
 		else{
 			L[i] = -(aA-bB);
 			D[i] =  1 + 2.0*aA + rfr*Dt + Dt/mu*(g[2][i]-g[3][N-i]);
 			U[i] = -(aA+bB);
 		}
-
-		//here
-		//if (i == 1) {//Boundary Condition
-		//	L[i] = 0;
-		//	D[i] = -2 * Bl / (1 + Dx / 2) + 1 + r * Dt + Bl + Bu + (Dt / mu)*(g[2][i] + g[2][N - i]);
-		//	U[i] = -Bu + (1 - Dx / 2) / (1 + Dx / 2)* Bl;
-		//}
-		//else if (i == N - 1) {//Boundary Condition
-		//	L[i] = -Bl + (1 + Dx / 2) / (1 - Dx / 2)* Bu;
-		//	D[i] = -2 * Bu / (1 - Dx / 2) + 1 + r * Dt + Bl + Bu + (Dt / mu)*(g[2][i] + g[2][N - i]);
-		//	U[i] = 0;
-		//}
-		//else {
-		//	L[i] = -Bl;
-		//	D[i] = 1 + r * Dt + Bl + Bu + (Dt / mu) *(g[2][i] + g[2][N - i]);
-		//	U[i] = -Bu;
-		//}
 	}
 
 
@@ -173,11 +142,10 @@ int main(int argc,char **argv){
 	}
 	delete [] g;
 
-	/*gettimeofday(&endTime, NULL); 
+	gettimeofday(&endTime, NULL); 
 	seconds = endTime.tv_sec - startTime.tv_sec; 
 	useconds = endTime.tv_usec - startTime.tv_usec; 
 	mtime = ((seconds) * 1000 + useconds/1000.0); 
-	cout << "Time elapsed was: " << mtime << " (milliseconds)" << endl;*/
-	printf("%f",(float(clock()) - float(t1))/ CLOCKS_PER_SEC);
+	cout << "Time elapsed was: " << mtime << " (milliseconds)" << endl;
 	return 0;
 }
